e6b47e9ca650c67f2f6c259c1576c50b11cf74ba
Karlson Drendel <kdrendel99@gmail.com>
2021-01-28 20:09:20 -0800

    Incorporate other branches of JS to include other test results.

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,36 @@ function check(){
   if (q5=="javascript") {a++}
   if (q6=="javascript") {a++}
 
-  document.write("Javascript Score = "+ a);
+  var b=0;
+  var q1=document.quiz.question1.value;
+  var q2=document.quiz.question2.value;
+  var q3=document.quiz.question3.value;
+  var q4=document.quiz.question4.value;
+  var q5=document.quiz.question5.value;
+  var q6=document.quiz.question6.value;
+  if (q1=="ruby") {b++}
+  if (q2=="ruby") {b++}
+  if (q3=="ruby") {b++}
+  if (q4=="ruby") {b++}
+  if (q5=="ruby") {b++}
+  if (q6=="ruby") {b++}
 
-}
+  var c=0;
+  var q1=document.quiz.question1.value;
+  var q2=document.quiz.question2.value;
+  var q3=document.quiz.question3.value;
+  var q4=document.quiz.question4.value;
+  var q5=document.quiz.question5.value;
+  var q6=document.quiz.question6.value;
+  if (q1=="python") {c++}
+  if (q2=="python") {c++}
+  if (q3=="python") {c++}
+  if (q4=="python") {c++}
+  if (q5=="python") {c++}
+  if (q6=="python") {c++}
+
+
+
+  document.write("Javascript Score = "+ a + "Ruby Score = "+ b + "Python Score = "+ c);
+  
+};
